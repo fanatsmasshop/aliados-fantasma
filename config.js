5e877a1bdd83f572ff86c0bb89aab542d57d0a74
Fantasmas Biker's Shop Cd Azteca <fanatsmasbikersecatepec@gmail.com>
2026-07-16 16:58:35 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ export const SUPABASE_URL =
   "https://cshjnpjqvflwmtypuyvc.supabase.co";
 
 export const SUPABASE_ANON_KEY =
-  "sb_publishable_jMUH4ytBXaX-yCt-QpgRkg_1
+  "sb_publishable_jMUH4ytBXaX-yCt-QpgRkg_16P_aaag";
   
 export const isConfigured = SUPABASE_URL.startsWith("https://") && !SUPABASE_URL.includes("PEGA_AQUI") && SUPABASE_ANON_KEY.length > 30 && !SUPABASE_ANON_KEY.includes("PEGA_AQUI");
