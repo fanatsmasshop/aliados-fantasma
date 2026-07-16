4c40a070187ff8fb2181b8b5b91a58d6a727dff7
Fantasmas Biker's Shop Cd Azteca <fanatsmasbikersecatepec@gmail.com>
2026-07-15 18:24:37 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// =================================================
+// JAVASCRIPT BÁSICO
+// =================================================
+
+// Por ahora la página no necesita funciones complicadas.
+
+// Este archivo queda preparado para agregar después:
+//
+// - Animaciones.
+// - Menú para celular.
+// - Formularios.
+// - Contadores.
+// - Estadísticas.
+
+console.log("Aliados Fantasma cargó correctamente.");
